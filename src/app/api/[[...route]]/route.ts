8471a6af7ade086cf7afadb035f4656335f9c9de
Mst-Takayama <masato.takayama@leber.jp>
2025-02-03 22:41:04 +0900

    リザルト画面apiの切り分け

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,11 +1,13 @@
 import { createAppWithRedis } from "@/app/utils/factory";
 import { handle } from "hono/vercel";
 import healthCheck from "@/features/healthCheck/route";
+import result from "@/features/result/route";
 // import { showRoutes } from 'hono/dev'
 
 const app = createAppWithRedis().basePath("/api");
 
 app.route("/health_check", healthCheck);
+app.route("/result", result);
 
 // ルーティングを確認したいときに使用
 // showRoutes(app);
---
+++ src/features/result/route.ts
@@ -0,0 +1,49 @@
+
+import { createAppWithRedis } from "@/app/utils/factory";
+
+const app = createAppWithRedis();
+
+app.post("/result", async (c) => {
+  const redis = c.get("redis");
+  try {
+    const { score, userName } = await c.req.json();
+
+    if (!score || !userName) {
+      throw new Error("Missing score or userName");
+    }
+
+    const result = {
+      score: score,
+      member: userName,
+    };
+
+    await redis.zadd("typing-score-rank", result);
+
+    return c.json({
+      message: "Score submitted successfully",
+    });
+  } catch (e) {
+    return c.json({ error: `Error: ${e}` }, 500);
+  }
+});
+
+app.get("/rank", async (c) => {
+  const redis = c.get("redis");
+  const result = await redis.zrange("typing-score-rank", 0, 9, {
+    rev: true,
+    withScores: true,
+  });
+
+  // [ { name: UserName, score: Score }, ... ] の形に変換
+  const rank: { name: string; score: number }[] = [];
+  for (let i = 0; i < result.length; i += 2) {
+    rank.push({
+      name: result[i] as string,
+      score: result[i + 1] as number,
+    });
+  }
+
+  return c.json(rank);
+});
+
+export default app;
