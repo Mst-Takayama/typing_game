e7a81b1741b62450edf48265c34b8e073c188c63
Mst-Takayama <masato.takayama@leber.jp>
2025-02-04 11:42:11 +0900

    構造化ログの導入

--- src/app/utils/factory.ts
+++ src/app/utils/factory.ts
@@ -1,14 +1,22 @@
 import { Hono } from "hono";
 import redis from "@/lib/redis";
+import { pinoLogger } from "hono-pino";
+import type { PinoLogger } from "hono-pino";
 
-type Env = {
+export type Env = {
   Variables: {
     redis: typeof redis;
-  }
-}
+    logger: PinoLogger;
+  };
+};
 
 export const createAppWithRedis = () => {
   const app = new Hono<Env>();
+  app.use(
+    pinoLogger({
+      pino: { level: "debug" },
+    })
+  );
   app.use(async (c, next) => {
     c.set("redis", redis);
     await next();
